a8ecddc8be25824adba1d23aec8eacc19a6d0795
CarlosEduardo7700 <rm552164@fiap.com.br>
2024-05-28 20:48:25 -0300

    Enviando para o GitHub

---
+++ src/main/java/br/com/oceandex/OceandexApplication.java
@@ -0,0 +1,13 @@
+package br.com.oceandex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OceandexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OceandexApplication.class, args);
+	}
+
+}

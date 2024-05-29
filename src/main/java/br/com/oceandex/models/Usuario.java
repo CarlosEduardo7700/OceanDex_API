a447d1e5707a252f8d2c95734ab0283eeef7b2d9
CarlosEduardo7700 <rm552164@fiap.com.br>
2024-05-28 21:07:41 -0300

    Criando a Entidade de Usuário.

---
+++ src/main/java/br/com/oceandex/models/Usuario.java
@@ -0,0 +1,36 @@
+package br.com.oceandex.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="OD_USUARIO")
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ID_USUARIO")
+    private Long id;
+
+    @Column(name = "NM_USUARIO", length = 200, nullable = false)
+    private String nome;
+
+    @Column(name = "EMAIL_USUARIO", length = 100, nullable = false)
+    private String email;
+
+    @Column(name = "SENHA_USUARIO", length = 16, nullable = false)
+    private String senha;
+
+    @Column(name = "IMG_USUARIO", length = 100)
+    private String fotoDePerfil;
+
+    @Column(name = "DT_CADASTRO", nullable = false)
+    private LocalDate dataDeCadastro;
+}

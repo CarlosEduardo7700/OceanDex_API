fbccf0bf8ec0b188203a0d8bbbedee4fc69084bc
CarlosEduardo7700 <rm552164@fiap.com.br>
2024-05-29 17:46:14 -0300

    Criando a Entidade de Dieta.

---
+++ src/main/java/br/com/oceandex/models/Dieta.java
@@ -0,0 +1,25 @@
+package br.com.oceandex.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "OD_DIETA")
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+public class Dieta {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ID_DIETA")
+    private Long id;
+
+    @Column(name = "NM_DIETA", length = 100, nullable = false)
+    private String nome;
+
+    @Column(name = "DESC_DIETA", length = 200, nullable = false)
+    private String descricao;
+}

56fd8c6382fa2f1e25d2a37f89ec54680594b1dd
CarlosEduardo7700 <rm552164@fiap.com.br>
2024-05-28 21:20:38 -0300

    Criando a Entidade de Animal.

---
+++ src/main/java/br/com/oceandex/models/Animal.java
@@ -0,0 +1,39 @@
+package br.com.oceandex.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "OD_ANIMAL")
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+public class Animal {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ID_ANIMAL")
+    private Long id;
+
+    @Column(name = "NM_ANIMAL", length = 200, nullable = false)
+    private String nomeComum;
+
+    @Column(name = "NM_CIENTIFICO_ANIMAL", length = 300, nullable = false)
+    private String nomeCientifico;
+
+    @Column(name = "DESC_ANIMAL", length = 300, nullable = false)
+    private String descricao;
+
+    @Column(name = "STATUS_ANIMAL", length = 20, nullable = false)
+    private String status;
+
+    @Column(name = "IMG_ANIMAL", length = 100, nullable = false)
+    private String imagem;
+
+    @Column(name = "DT_CAD_ANIMAL", nullable = false)
+    private LocalDate dataDeCadastro;
+}

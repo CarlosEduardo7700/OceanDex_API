f0c11cc700ffd61b1c6adf2c94c50ca359ab519c
CarlosEduardo7700 <rm552164@fiap.com.br>
2024-06-03 17:03:29 -0300

    Criando o método para buscar todos os Usuários e buscar um pelo ID.

--- src/main/java/br/com/oceandex/controllers/UsuarioController.java
+++ src/main/java/br/com/oceandex/controllers/UsuarioController.java
@@ -1,18 +1,17 @@
 package br.com.oceandex.controllers;
 
 import br.com.oceandex.models.Usuario;
-import br.com.oceandex.models.dtos.CadastrarUsuarioDto;
-import br.com.oceandex.models.dtos.DetalhesUsuarioDto;
+import br.com.oceandex.models.dtos.*;
 import br.com.oceandex.repositories.UsuarioRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("usuarios")
@@ -29,5 +28,23 @@ public class UsuarioController {
         repository.save(usuario);
         var uri = builder.path("usuarios/{id}").buildAndExpand(usuario.getId()).toUri();
         return ResponseEntity.created(uri).body(new DetalhesUsuarioDto(usuario));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ListagemUsuarioDto>> buscarTudo(Pageable pageable) {
+
+        var lista = repository.findAll(pageable)
+                .stream().map(ListagemUsuarioDto::new).toList();
+
+        if (!lista.isEmpty())
+            return ResponseEntity.ok(lista);
+        else
+            return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<DetalhesUsuarioDto> buscarPorId(@PathVariable("id") Long id) {
+        var usuario = repository.getReferenceById(id);
+        return ResponseEntity.ok(new DetalhesUsuarioDto(usuario));
     }
 }
---
+++ src/main/java/br/com/oceandex/models/dtos/ListagemUsuarioDto.java
@@ -0,0 +1,19 @@
+package br.com.oceandex.models.dtos;
+
+import br.com.oceandex.models.Usuario;
+
+public record ListagemUsuarioDto(
+        Long id,
+        String nome,
+        String email,
+        String fotoDePerfil
+) {
+    public ListagemUsuarioDto(Usuario usuario) {
+        this(
+                usuario.getId(),
+                usuario.getNome(),
+                usuario.getEmail(),
+                usuario.getFotoDePerfil()
+        );
+    }
+}
